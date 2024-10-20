a335685a1a8a34282da50d4e2998adbf9db65aeb
pugaizai <i@pugai.life>
2024-10-20 22:46:15 +0800

    chore: update dark theme

--- lib/screen/reader/themes/dark.dart
+++ lib/screen/reader/themes/dark.dart
@@ -3,9 +3,10 @@ import 'package:flutter/material.dart';
 final darkTheme = ThemeData(
     useMaterial3: true,
     colorScheme: const ColorScheme.light(
-        primary: Color(0xFF3A7A5F),
-        primaryContainer: Color(0xFF1E3D30),
-        secondary: Color(0xFF5C6663),
-        onSurface: Color(0xFFE0E3E1),
-        surface: Color(0xFF121212),
-        outline: Color(0xFF2C2F2D)));
+      primary: Color(0xFF607D8B), // 深灰色作为主要强调色
+      primaryContainer: Color(0xFF455A64), // 更深的灰色作为次要强调色
+      secondary: Color(0xFF78909C), // 稍浅的灰色作为次要颜色
+      onSurface: Color(0xFFE0E0E0), // 浅灰色，用于文本
+      surface: Color(0xFF121212), // 深灰色，稍微柔和的黑色背景
+      outline: Color(0xFF3A3A3A), // 深灰色轮廓
+    ));
